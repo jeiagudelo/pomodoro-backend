a4dd463b383554c6c6beb8c493d1068eafdd773e
jeiagudelo <jeisson97070@gmail.com>
2025-04-14 14:03:13 -0600

    Subida completa del backend

--- models/session.py
+++ models/session.py
@@ -7,4 +7,5 @@ class Session(db.Model):
     type = db.Column(db.String(20), nullable=False)
     start_time = db.Column(db.DateTime, default=datetime.utcnow)
     end_time = db.Column(db.DateTime)
+    
     completed = db.Column(db.Boolean, default=False)
